a96a6e894b439603155c588c08963d4378b0fbba
andres2389 <aframirez@soy.sena.edu.co>
2024-03-26 22:26:18 -0500

    new project portafolio-nodejs the firts commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,5 +4,7 @@ const router =  Router()
 router.get('/', (req, res) => res.render('index',{ title: 'Bienvenido-home'}))
 router.get('/about', (req, res) => res.render('about',{title: 'About'}))
 router.get('/contact', (req, res) => res.render('contact' , {title : 'Contact'}))
+router.get('/skills', (req, res) => res.render('skills' , {title : 'skills'}))
+router.get('/proyects', (req, res) => res.render('proyects' , {title : 'proyects'}))
 
 export default router
